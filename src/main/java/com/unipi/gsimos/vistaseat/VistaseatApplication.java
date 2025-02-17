515f34af8890d3f3c024954fe1f883499fa305ac
GiorgosSimos <giorgos_Simos@windowslive.com>
2025-02-17 15:26:16 +0200

    Initial commit

---
+++ src/main/java/com/unipi/gsimos/vistaseat/VistaseatApplication.java
@@ -0,0 +1,13 @@
+package com.unipi.gsimos.vistaseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VistaseatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VistaseatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unipi/gsimos/vistaseat/VistaseatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipi.gsimos.vistaseat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VistaseatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
