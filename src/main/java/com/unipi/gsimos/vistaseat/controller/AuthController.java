22d00a3b7ad2fd6afb26c4f9d18b87240d4e1af0
GiorgosSimos <giorgos_Simos@windowslive.com>
2025-05-05 19:23:42 +0300

    - Minor changes

--- src/main/java/com/unipi/gsimos/vistaseat/controller/AuthController.java
+++ src/main/java/com/unipi/gsimos/vistaseat/controller/AuthController.java
@@ -1,16 +1,7 @@
 package com.unipi.gsimos.vistaseat.controller;
 
-import com.unipi.gsimos.vistaseat.dto.LoginRequestDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,22 +18,4 @@ public class AuthController {
     }
 
 
-    /*@PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequestDto loginRequestDto) {
-
-        try{
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                       loginRequestDto.getEmail(),
-                       loginRequestDto.getPassword()
-                    )
-            );
-
-            UserDetails user = (UserDetails) authentication.getPrincipal();
-            return ResponseEntity.ok("Login successful for: " + user.getUsername());
-            // Later, here you can return a JWT token if needed
-        } catch (BadCredentialsException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
-        }
-    }*/
 }
